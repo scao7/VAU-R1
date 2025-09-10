3057af03302bb6976c92a2bee855549599fa59ad
scao7 <scao7@crimson.ua.edu>
2025-09-10 14:47:29 -0500

    add change

--- scripts/training/run_grpo_video_qa.sh
+++ scripts/training/run_grpo_video_qa.sh
@@ -2,48 +2,43 @@ export WANDB_NAME=$(basename $0)_$(date +"%Y%m%d_%H%M%S")
 export PYTHONPATH=".:$PYTHONPATH"
 OUTDIR=./checkpoints/$WANDB_NAME
 
-# export DEBUG_MODE="true"
+export DEBUG_MODE="true"
 export LOG_PATH="./logs/${WANDB_NAME}.log"
 
 export TOKENIZERS_PARALLELISM=false
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 
-# export OMP_NUM_THREADS=1
-# export DISABLE_ADDMM_CUDA_LT=1
-# export TORCH_CUDNN_USE_HEURISTIC_MODE_B=1
-
+export OMP_NUM_THREADS=1
+export DISABLE_ADDMM_CUDA_LT=1
+export TORCH_CUDNN_USE_HEURISTIC_MODE_B=1
+export TORCH_CUDA_ARCH_LIST="86"  
 OUTDIR=./checkpoints/$WANDB_NAME
 
-torchrun \
-    --nproc_per_node=1 \
-    --nnodes=1 \
-    --node_rank=0 \
-    --master_addr=127.0.0.1 \
-    --master_port=10668 \
-    src/open_r1/grpo_qa.py \
-    --deepspeed training_scripts/zero3_offload.json \
+
+    
+    python src/open_r1/grpo_qa.py \
+    --deepspeed scripts/training/zero3_offload.json \
     --output_dir $OUTDIR \
-    --model_name_or_path ./path/to/model/ \
-    --train_data_path /path/to/train.csv \
-    --eval_data_path /path/to/val.csv \
-    --train_video_folder /path/to/train/videos \
-    --eval_video_folder /path/to/val/videos \
+    --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
+    --train_data_path /data/Shengting/AFTER_GRADUATE/VAU-R1/annotations/ucf_subset_train.csv \
+    --eval_data_path /data/Shengting/AFTER_GRADUATE/VAU-R1/annotations/ucf_subset_val.csv \
+    --train_video_folder /data/Shengting/AFTER_GRADUATE/VAU-R1-old/video_data/train \
+    --eval_video_folder /data/Shengting/AFTER_GRADUATE/VAU-R1-old/video_data/val \
     --dataset_name All \
     --max_prompt_length 1024 \
     --max_completion_length 1024 \
-    --num_generations 4 \
-    --per_device_train_batch_size 1 \
+    --num_generations 2 \
+    --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 1 \
     --logging_steps 1 \
-    --bf16 \
     --torch_dtype bfloat16 \
     --data_seed 42 \
     --gradient_checkpointing true \
-    --attn_implementation flash_attention_2 \
+    --attn_implementation sdpa \
     --num_train_epochs 1 \
     --run_name $WANDB_NAME \
     --report_to tensorboard \
     --save_steps 100 \
     --save_total_limit 3 \
-    --save_only_model true
+
 
